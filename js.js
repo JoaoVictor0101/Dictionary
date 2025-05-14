b8ac2f70fbe9a5553e33493f7eea0146cc8c1e20
J.victor <victordev0101@gmail.com>
2025-05-14 20:26:52 -0300

    fixing the initial page and the style

--- js.js
+++ js.js
@@ -5,8 +5,19 @@ let contentContainer = document.getElementById("contentContainer")
 const audioDIv = document.getElementById("audioDiv");
 let track = 0
 var api = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-
+var api2 ="https://random-word-api.herokuapp.com/word?number=1"
 //why the toLowerCase() didn''t work
+async function getRandomWord() {
+   try{
+    const response2 = await fetch(`${api2}`)
+    const data2 = await response2.json()
+    console.log(data2)
+   }catch{
+     console.error('Error fetching word:', error);
+   }
+} 
+
+window.onload = getRandomWord // how it works?
 
 async function searchMeaning(){
     let getTheWord = SearchBar.value.toLowerCase();
@@ -32,23 +43,23 @@ async function searchMeaning(){
              });;
             });
         
-            const audio = data[0].phonetics[0].audio;
-            const fetchAudio = await fetch(audio);
-            const covertAudio = await fetchAudio.blob();
-            const AudioUrl= URL.createObjectURL(covertAudio);
+            // add a condition to audio to avoid erros in the search like "dogfights"
+
+            // const audio = data[0].phonetics[0].audio;
+            // const fetchAudio = await fetch(audio);
+            // const covertAudio = await fetchAudio.blob();
+            // const AudioUrl= URL.createObjectURL(covertAudio);
             
-            audioDIv.src = AudioUrl;
-            audioDIv.controls = true;
+            // audioDIv.src = AudioUrl;
+            // audioDIv.controls = true;
         
-            contentContainer.appendChild(audioDIv);
+            // contentContainer.appendChild(audioDIv);
 
 
           const coversion = JSON.stringify(allMeanings);
         
         
           divDefinition.textContent= coversion.replace(/[{}\[\]]/g,'');
-
-           
 
        }catch(e){
             console.log(e.message);
